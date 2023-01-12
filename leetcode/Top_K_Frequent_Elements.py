06c372c45aa34ce35e9b09c74112926192951574
Youn <pleiadex.dev@gmail.com>
2023-01-12 22:15:34 +0900

    new idea using bucket sort by neetcode

--- leetcode/Top_K_Frequent_Elements.py
+++ leetcode/Top_K_Frequent_Elements.py
@@ -1,15 +1,22 @@
 from typing import List
-from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-      hmap = defaultdict(int)
+      count = {}
+      freq = [[] for _ in range(len(nums) + 1)]
 
+      # count using a hash map
       for num in nums:
-        hmap[num] += 1
+        count[num] = 1 + count.get(num, 0)
+      
+      # bucket sort -> Time O(n) Space O(n)
+      for num, cnt in count.items():
+        freq[cnt].append(num)
       
       res = []
-      count = sorted(hmap.items(), key=lambda tup: -tup[1])
-      for i in range(k):
-        res.append(count[i][0])
 
-      return res
+      for i in range(len(freq)-1, 0, -1):
+        res += freq[i]
+        if len(res) == k:
+          return res
+
+      return []
