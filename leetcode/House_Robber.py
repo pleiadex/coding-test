02bc1f0d319a5e726c0c99bcb516538fe9a3198e
Youn <pleiadex.dev@gmail.com>
2023-01-13 15:03:02 +0900

    update the code to the optimal and straightforward one

--- leetcode/House_Robber.py
+++ leetcode/House_Robber.py
@@ -1,16 +1,12 @@
 from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
-      money = [0]
-      money.append(nums[0])
-      if len(nums) == 1:
-        return money[-1]
 
-      money.append(max(nums[0], nums[1]))
-      if len(nums) == 2:
-        return money[-1]
+      # A smart way to eliminate any branches by edge cases
+      rob1, rob2 = 0, 0
+
+      for n in nums:
+        money = max(rob1 + n, rob2)
+        rob1, rob2 = rob2, money
       
-      for i in range(2, len(nums)):
-        money.append(max(money[-2] + nums[i], money[-1]))
-
-      return money[-1]
+      return rob2
