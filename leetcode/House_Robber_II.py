314fb4968323fadc01c36b47ae7beaf619a72191
Youn <pleiadex.dev@gmail.com>
2023-01-13 15:42:01 +0900

    better and cleaner code by Neetcode

--- leetcode/House_Robber_II.py
+++ leetcode/House_Robber_II.py
@@ -1,22 +1,13 @@
 from typing import List
 class Solution:
     def rob(self, nums: List[int]) -> int:
+      return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
 
-      if len(nums) == 1:
-        return nums[0]
-
+    def helper(self, nums: List[int]) -> int:
       rob1, rob2 = 0, 0
-      maxProfit = 0
 
-      for n in nums[1:]:
+      for n in nums:
         money = max(rob1 + n, rob2)
-        maxProfit = max(money, maxProfit)
         rob1, rob2 = rob2, money
       
-      rob1, rob2 = 0, 0
-      for n in nums[:-1]:
-        money = max(rob1 + n, rob2)
-        maxProfit = max(money, maxProfit)
-        rob1, rob2 = rob2, money
-      
-      return maxProfit
+      return rob2
