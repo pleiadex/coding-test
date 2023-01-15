3af0b6dcbce18fa258456b0b514be658216fe95b
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-16 00:30:17 +0900

    add another dp solution

--- leetcode/Count_Bits.py
+++ leetcode/Count_Bits.py
@@ -3,7 +3,11 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
 
       res = [0]
+      offset = 1
 
       for i in range(1, n + 1):
-        res.append(i % 2 + res[i//2])
+        # res.append(i % 2 + res[i//2])
+        if offset * 2 == i:
+          offset = i
+        res.append(1 + res[i - offset])
       return res
