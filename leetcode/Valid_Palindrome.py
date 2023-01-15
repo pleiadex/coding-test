a14fbb930cb2c1ab68ef67269391e02af2e11d34
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-15 16:42:06 +0900

    cleaner code with less branching

--- leetcode/Valid_Palindrome.py
+++ leetcode/Valid_Palindrome.py
@@ -2,21 +2,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
       l, r = 0, len(s) - 1
 
-      while l < r:
-        # skip a non alphabet character
-        while l < len(s) and not s[l].isalnum():
-          l += 1
-        while r >= 0 and not s[r].isalnum():
+      while l <= r:
+        if s[l].isalnum():
+          while not s[r].isalnum():
+            r += -1
+          if s[l].lower() != s[r].lower():
+            return False
           r += -1
-        
-        # exit the loop if l or r is out of range
-        if l == len(s) or r == -1:
-          break
-
-        if s[l].lower() != s[r].lower():
-          return False
-
-        else:
-          l += 1
-          r += -1
+        l += 1
       return True
