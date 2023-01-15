c4acb2566f2547bf7e586c51064f62f5776ce31b
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-16 00:54:04 +0900

    amazing bit manipulation trick

--- leetcode/Number_of_1_Bits.py
+++ leetcode/Number_of_1_Bits.py
@@ -1,8 +1,10 @@
 class Solution:
   def hammingWeight(self, n: int) -> int:
     result = 0
-    for i in range(32):
-        result += n & 1
-        n >>= 1
+    while n:
+      n = n & (n - 1)
+      result += 1
+      # result += n & 1
+      # n >>= 1
 
     return result
