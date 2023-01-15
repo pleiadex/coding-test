1fd19e3bdffa5abb214ff45addd3dfc7abcca57c
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-16 00:17:20 +0900

    merged the edge case when n is 1

--- leetcode/Count_Bits.py
+++ leetcode/Count_Bits.py
@@ -2,10 +2,8 @@ from typing import List
 class Solution:
     def countBits(self, n: int) -> List[int]:
 
-      res = [0, 1]
-      if n < 2:
-        return res[:n + 1]
+      res = [0]
 
-      for i in range(2, n + 1):
+      for i in range(1, n + 1):
         res.append(i % 2 + res[i//2])
       return res
