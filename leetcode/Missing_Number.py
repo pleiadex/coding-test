6559a86ff2443dd0d1adbb2ad8bda4a57f0e5825
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-15 19:29:53 +0900

    the most elegant and easiest to code up; sum([0..n]) - sum([0..n-1]) = n

--- leetcode/Missing_Number.py
+++ leetcode/Missing_Number.py
@@ -1,12 +1,5 @@
 from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-      res = 0
-
-      for n in nums:
-        res ^= n
-      
-      for i in range(len(nums) + 1):
-        res ^= i
-
-      return res
+      maxValue = len(nums)
+      return maxValue * (maxValue + 1) // 2 - sum(nums)
