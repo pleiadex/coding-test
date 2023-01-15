d4d7932c83b51c3311dcab7764add83e544f7575
Sungyoun Seo <pleiadex.dev@gmail.com>
2023-01-15 19:11:41 +0900

    the better approach with the intuitions of the associativity of xor and n^n = 0, n^0=n

--- leetcode/Missing_Number.py
+++ leetcode/Missing_Number.py
@@ -1,18 +1,12 @@
 from typing import List
-from math import log, ceil
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
       res = 0
 
-      # This does not look clean
-      if len(nums) == 1:
-        return nums[0] ^ 1
-
-      for num in nums:
-        res ^= num
+      for n in nums:
+        res ^= n
       
-      closestPowerOf2 = 2**ceil(log(len(nums) + 1 ,2))
-      for num in range(len(nums) + 1, closestPowerOf2):
-        res ^= num
+      for i in range(len(nums) + 1):
+        res ^= i
 
       return res
