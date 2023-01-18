fbb2693fcef4944680d4f928dfc52e19172f2867
Youn <pleiadex.dev@gmail.com>
2023-01-18 14:54:33 +0900

    recursion

--- leetcode/Clone_Graph.py
+++ leetcode/Clone_Graph.py
@@ -5,35 +5,22 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-from collections import deque
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        if not node:
-            return None
+        hmap = {}
 
-        queue = deque([node])
-        visited = [False] * 101
-        hmap = {node: Node(node.val)}
-        res = hmap[node]
+        def clone(node):
+            if node in hmap:
+              return hmap[node]
+            
+            hmap[node] = Node(node.val)
+            for n in node.neighbors:
+              hmap[node].neighbors.append(clone(n))
 
-        while queue:
-            curr = queue.popleft()
-            if curr and not visited[curr.val]:
-                visited[curr.val] = True
-                for n in curr.neighbors:
-                    queue.append(n)
+            return hmap[node]
 
-                if not curr in hmap:
-                    hmap[curr] = Node(curr.val)
-                for n in curr.neighbors:
-                    if not n in hmap:
-                        hmap[n] = Node(n.val)
-                    hmap[curr].neighbors.append(hmap[n])
-        return res
-
-        
+        return clone(node) if node else None
