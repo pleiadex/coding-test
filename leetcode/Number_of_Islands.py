8f878d97e6206b4d4e4be6ed3f5c3b54c4a22ff5
Youn <pleiadex.dev@gmail.com>
2023-01-20 20:32:24 +0900

    the cleaner dfs solution

--- leetcode/Number_of_Islands.py
+++ leetcode/Number_of_Islands.py
@@ -4,7 +4,8 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
       def visit(i, j):
         # out of range
-        if i < 0 or len(grid) <= i or j < 0 or len(grid[0]) <= j:
+        # if i < 0 or len(grid) <= i or j < 0 or len(grid[0]) <= j:
+        if not i in range(len(grid)) or not j in range(len(grid[0])):
           return
 
         if grid[i][j] == '0':
@@ -15,7 +16,6 @@ class Solution:
         visit(i - 1, j)
         visit(i, j + 1)
         visit(i, j - 1)
-
       
       res = 0
       for i in range(len(grid)):
