beb22f40a9e8ea76f93c2689e7259e090bf506ca
Youn <pleiadex.dev@gmail.com>
2023-01-26 21:58:01 +0900

    first trial

---
+++ hackerrank/Counting_Sort_2.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Write your code here
+    count = [0] * 100
+    for i in arr:
+      count[i] += 1
+
+    res = []    
+    
+    for i, cnt in enumerate(count):
+      for _ in range(cnt):
+        res.append(i)
+    
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
